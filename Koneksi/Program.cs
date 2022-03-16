d14b427351a4b482cdd9be59ec05f633cf3c3ad9
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-03-16 14:09:04 +0700

    Add project files.

---
+++ Koneksi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting() 
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=DESKTOP-KVHUS77\\RIDWANAM;database=ProdiTI;User ID=sa;Password=riamima")
+            ) 
+            {
+                con.Open();
+                Console.WriteLine("Berhasil terhubung ke database!");
+            }
+        }
+    }
+}
